11658787684b5f26775563a46d5ae7545bb09e41
Maxime <maxime.guigourez@gmail.com>
2019-09-26 15:09:07 +0200

    début de UI

--- src/Vue_Run/Window.java
+++ src/Vue_Run/Window.java
@@ -3,38 +3,48 @@ package Vue_Run;
 import Modele.DB.*;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
+public class Window extends JFrame {
 
-public class Window {
-
-    private JPanel panel1;
     private JList<String> list1;
-    private JTextField textField1;
-    private JButton button1;
-    private JPanel panel2;
+    private JPanel panel1;
+    private JPanel description;
+
+    public Window(String nom){
+
+        this.panel1 = new JPanel();
+        this.description = new JPanel();
+
+        this.panel1.setLayout(new GridLayout(1,1) );
+        this.description.setLayout(new GridLayout(2,1));
 
 
-    public Window(){
-    	Base base = new Base();
+
+
+
+        Base base = new Base();
         DefaultListModel<String> def = new DefaultListModel<String>();
     	this.list1 = new JList<String>(def);
 
         for(int i =0;i<=base.selectAll().size()-1;i++){
             def.addElement(base.selectAll().get(i));
+            def.addElement(" ");
             System.out.println(base.selectAll().get(i));
         }
 
-        panel1.add(list1);
+        this.panel1.add(list1);
+
+        this.setContentPane(this.panel1);
+        this.setName(nom);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(1000,700));
+        this.pack();
+        this.setVisible(true);
     };
 
     public static void main(String args[]){
-        JFrame frame = new JFrame("Medical Location App");
-        frame.setContentPane(new Window().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        Window win  = new Window("Medical Location App");
     }
 
 }
